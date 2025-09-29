2211d61ad96a3a7111ac8cf1f9724aeb776b80ae
RMS-Ronaldo <ronaldo7472_markes@hotmail.com>
2025-09-29 23:01:22 +0000

    feat: carrinho de compra

--- exercicio_pdv/item_compra.dart
+++ exercicio_pdv/item_compra.dart
@@ -1,16 +1,21 @@
 import 'produto.dart';
-
 class ItemCompra{
     Produto produto;
     int quantidade;
 
     ItemCompra(this.produto, this.quantidade);
-    
+
     double calcularSubTotal(){
         return quantidade * produto.valorUnitario;
     }
 
-    @overrideString toString(){
-        return '${produto.nome} - ${quantidade} unid. X R\$ ${produto.valorUnitario.toStringAsfixed(2)} = R\$ ${calcularSubTotal().toStringAsFixed(2)}';
+    @override
+    String toString(){
+        return '''
+        ${produto.nome} - ${quantidade} unid. X 
+        R\$ ${produto.valorUnitario.toStringAsFixed(2)} =
+        R\$ ${calcularSubTotal().toStringAsFixed(2)}
+        ''';
+        
     }
 }
